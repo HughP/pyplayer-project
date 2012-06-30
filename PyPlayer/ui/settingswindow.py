ac7c7b64a6a060e5ee58a8aeb5c3b6651db86bb8
raymanos <raymanos00@gmail.com>
2012-06-30 15:25:11 +0100

    ??????? ?????? testmerc ??? ????? %)

---
+++ PyPlayer/ui/settingswindow.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'h:\pyplayer\ui\settingswindow.ui'
+#
+# Created: Fri Jun 22 19:11:27 2012
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_SettingsWindow(object):
+    def setupUi(self, SettingsWindow):
+        SettingsWindow.setObjectName(_fromUtf8("SettingsWindow"))
+        SettingsWindow.resize(404, 292)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(SettingsWindow)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.tabWidget = QtGui.QTabWidget(SettingsWindow)
+        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
+        self.tab = QtGui.QWidget()
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.lineEdit = QtGui.QLineEdit(self.tab)
+        self.lineEdit.setGeometry(QtCore.QRect(20, 40, 191, 20))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.label = QtGui.QLabel(self.tab)
+        self.label.setGeometry(QtCore.QRect(10, 20, 141, 16))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.toolButton = QtGui.QToolButton(self.tab)
+        self.toolButton.setGeometry(QtCore.QRect(220, 40, 25, 19))
+        self.toolButton.setObjectName(_fromUtf8("toolButton"))
+        self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.checkBox = QtGui.QCheckBox(self.tab_2)
+        self.checkBox.setGeometry(QtCore.QRect(20, 130, 121, 17))
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.label_2 = QtGui.QLabel(self.tab_2)
+        self.label_2.setGeometry(QtCore.QRect(20, 20, 141, 16))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.lineEdit_2 = QtGui.QLineEdit(self.tab_2)
+        self.lineEdit_2.setGeometry(QtCore.QRect(30, 40, 113, 20))
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.label_3 = QtGui.QLabel(self.tab_2)
+        self.label_3.setGeometry(QtCore.QRect(20, 70, 121, 16))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.lineEdit_3 = QtGui.QLineEdit(self.tab_2)
+        self.lineEdit_3.setGeometry(QtCore.QRect(30, 90, 113, 20))
+        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
+        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
+        self.verticalLayout.addWidget(self.tabWidget)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+        self.pushButton = QtGui.QPushButton(SettingsWindow)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.verticalLayout_2.addWidget(self.pushButton)
+
+        self.retranslateUi(SettingsWindow)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(SettingsWindow)
+
+    def retranslateUi(self, SettingsWindow):
+        SettingsWindow.setWindowTitle(QtGui.QApplication.translate("SettingsWindow", "Настройки", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("SettingsWindow", "Путь к коллекции", None, QtGui.QApplication.UnicodeUTF8))
+        self.toolButton.setText(QtGui.QApplication.translate("SettingsWindow", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QtGui.QApplication.translate("SettingsWindow", "Сис настройки", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkBox.setText(QtGui.QApplication.translate("SettingsWindow", "Скроблить", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("SettingsWindow", "Имя пользователя", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("SettingsWindow", "Пароль пользователя", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QtGui.QApplication.translate("SettingsWindow", "Last.FM", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("SettingsWindow", "OK", None, QtGui.QApplication.UnicodeUTF8))
+
